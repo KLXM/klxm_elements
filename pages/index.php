05dd7da3b54fa5943f01c840b71dc08a3137df5e
Thomas Skerbis <thomas.skerbis@mac.com>
2026-06-13 16:40:11 +0200

    Modulseite entfernt und Admin-Zugriff erzwungen

--- pages/index.php
+++ pages/index.php
@@ -1,5 +1,11 @@
 <?php
 
 $addon = rex_addon::get('klxm_elements');
+
+if (!rex::getUser() || !rex::getUser()->isAdmin()) {
+	echo rex_view::error('Nur Administratoren dürfen diese Seite aufrufen.');
+	return;
+}
+
 echo rex_view::title($addon->getProperty('name'));
 rex_be_controller::includeCurrentPageSubPath();
